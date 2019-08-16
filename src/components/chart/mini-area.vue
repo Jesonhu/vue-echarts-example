f3d419b512184c442669952bf7687ad92de207d9
Jeson <jesonhu_web@163.com>
2019-08-16 18:06:16 +0800

    chore(mini-area): 组件添加中.

---
+++ src/components/chart/mini-area.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="mini-area">
+    <v-echarts></v-echarts>
+  </div>
+</template>
+
+<script>
+ 
+export default {
+  name: 'mini-area',
+  data() {
+    return {}
+  }
+}
+</script>
